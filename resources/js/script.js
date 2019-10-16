94533f2417523d71e690aaeaae9e762cd859a4d8
Tamas Val <valtamas@hotmail.com>
2019-10-16 15:09:14 +0200

    Video section

--- resources/js/script.js
+++ resources/js/script.js
@@ -60,50 +60,79 @@ $(document).ready(function() {
       }
     });
 
+    $(".js--wp-1").waypoint(
+      function(direction) {
+        $(".js--wp-1").addClass("animated fadeInDown");
+      },
+      {
+        offset: "80%"
+      }
+    );
+    
+    
+    $(".js--wp-2").waypoint(function(direction){
+      var element =  document.querySelector('.js--wp-2');
+      element.classList.add('animated', 'slideInRight');
+      element.addEventListener('animationend', function(){
+        var element =  document.querySelector('.js--wp-3');
+        element.classList.add('animated', 'slideInRight');
+        element.addEventListener('animationend', function(){
+          var element =  document.querySelector('.js--wp-4');
+          element.classList.add('animated', 'slideInRight');
+          element.addEventListener('animationend', function(){
+            var element =  document.querySelector('.js--wp-5');
+            element.classList.add('animated', 'slideInRight');
+          })
+          })    
+      })
+      
+    },{
+      offset: "60%"
+    });
 
-  $(".js--wp-1").waypoint(
-    function(direction) {
-      $(".js--wp-1").addClass("animated fadeIn");
-    },
-    {
-      offset: "80%"
-    }
-  );
-  $(".js--wp-2").waypoint(
-    function(direction) {
-      $(".js--wp-2").addClass("animated fadeIn");
-    },
-    {
-      offset: "80%"
-    }
-  );
-  $(".js--wp-3").waypoint(
-    function(direction) {
-      $(".js--wp-3").addClass("animated fadeIn");
-    },
-    {
-      offset: "80%"
-    }
-  );
-  $(".js--wp-4").waypoint(
-    function(direction) {
-      $(".js--wp-4").addClass("animated fadeIn");
-    },
-    {
-      offset: "80%"
-    }
-  );
-  $(".js--wp-5").waypoint(
-    function(direction) {
-      $(".js--wp-5").addClass("animated fadeIn");
-    },
-    {
-      offset: "80%"
-    }
-  );
   $(".js--wp-6").waypoint(
     function(direction) {
       $(".js--wp-6").addClass("animated fadeIn");
+    },
+    {
+      offset: "80%"
+    }
+  );
+  $(".js--wp-7").waypoint(
+    function(direction) {
+      $(".js--wp-7").addClass("animated fadeIn");
+    },
+    {
+      offset: "80%"
+    }
+  );
+  $(".js--wp-8").waypoint(
+    function(direction) {
+      $(".js--wp-8").addClass("animated fadeIn");
+    },
+    {
+      offset: "80%"
+    }
+  );
+  $(".js--wp-9").waypoint(
+    function(direction) {
+      $(".js--wp-9").addClass("animated fadeIn");
+    },
+    {
+      offset: "80%"
+    }
+  );
+  $(".js--wp-10").waypoint(
+    function(direction) {
+      $(".js--wp-10").addClass("animated fadeIn");
+    },
+    {
+      offset: "80%"
+    }
+  );
+  $(".js--wp-11").waypoint(
+    function(direction) {
+      $(".js--wp-11").addClass("animated fadeIn");
     },
     {
       offset: "80%"
