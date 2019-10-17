abc7df162dcbb5b0af11c1d5dcbbd07433540cdc
Tamas Val <valtamas@hotmail.com>
2019-10-17 15:02:37 +0200

    Mobile navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,6 +12,25 @@ $(document).ready(function() {
     }
   );
 
+  
+  
+  $('.js--nav-icon').click(function(){
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+    
+    nav.slideToggle(200);
+    if(icon.hasClass('ion-navicon-round')){
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+    } else {
+     icon.addClass('ion-navicon-round');
+     icon.removeClass('ion-close-round');
+    }
+ });  
+  
+  
+  
+  
   $(".js--scroll-to-pictures").click(function() {
     $("html, body").animate({ scrollTop: $(".js--section-pictures").offset().top }, 1000);
   });
